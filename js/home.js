81fbce50696bd27c29e3b218e08372a921610262
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2020-10-05 00:04:11 +0200

    work on projects started

--- js/home.js
+++ js/home.js
@@ -5,4 +5,22 @@ var rellax = new Rellax('.rellax', {
      round: true,
      vertical: true,
      horizontal: false,
+     breakpoints: [576, 768, 1201],
+
 });
+
+
+const newUrl1 = "../subpages/project_1.html";
+
+const projectBoxes = document.querySelectorAll('.work_wrapper > div .img');
+const projects_scrim = document.querySelectorAll('.projects_scrim');
+// rellax.destroy();
+
+projectBoxes[0].addEventListener('click', () => {
+     projects_scrim[0].classList.add("active");
+
+     setTimeout(() => {
+          window.location = newUrl1;
+
+     }, 1700);
+})
